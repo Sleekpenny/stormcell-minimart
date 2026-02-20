18a45f33d866be2a2032069addfc23477aacaede
Penny <macphantomsoft@gmail.com>
2026-02-20 02:43:54 +0100

    fix: correct routing syntax

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./shared/layouts/master-layout/master-layout.component').then((m) => m.MasterLayoutComponent),
     loadChildren: () => import('./modules/routes').then((m) => m.routes),
-}
+  }  
 ];
